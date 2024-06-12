a5b8b64d0b3732b60dce916a50f251897e790436
Daniele Muffato <daniele.muffato@studio.unibo.it>
2024-06-12 23:07:28 +0200

    left only main file

--- db-2024/app/src/main/java/db_lab/App.java
+++ db-2024/app/src/main/java/db_lab/App.java
@@ -1,29 +1,12 @@
 package db_lab;
 
-import db_lab.data.DAOException;
-import db_lab.data.DAOUtils;
-import db_lab.model.Model;
-import java.sql.SQLException;
+import javax.swing.JFrame;
 
 public final class App {
 
-    public static void main(String[] args) throws SQLException {
-        // If you want to get a feel of the application before having implemented
-        // all methods, you can pass the controller a mocked model instead:
-        //
-        // var model = Model.mock();
-        var connection = DAOUtils.localMySQLConnection("Tessiland", "root", "mysql+0A");
-        var model = Model.fromConnection(connection);
-        var view = new View(() -> {
-            // We want to make sure we close the connection when we're done
-            // with our application.
-            try {
-                connection.close();
-            } catch (Exception e) {
-            }
-        });
-        var controller = new Controller(model, view);
-        view.setController(controller);
-        controller.userRequestedInitialPage();
+    public static void main(String[] args) {
+        var frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(120, 100);
     }
 }
